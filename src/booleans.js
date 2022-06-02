8d544fb953fe668aef87cfd1c43f736853b29847
Tom Mills <t.mills124@gmail.com>
2022-06-02 17:50:19 +0100

    solved boolean problems 8-11

--- src/booleans.js
+++ src/booleans.js
@@ -35,19 +35,19 @@ function isEqual(a, b) {
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a > b;
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a <= b;
 };
 
 function isOdd(a) {
-  // your code here
+  return (a % 2) === 1;
 };
 
 function isEven(a) {
-  // your code here
+  return (a % 2) === 0;
 };
 
 function isSquare(a) {
